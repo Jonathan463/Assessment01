d08c00f9e302c9e59251698433a61f0b8d82e75a
JonathanChidubem <godson@getrova.com>
2024-05-18 20:41:10 +0100

    implemented first Assessment to get smallest non occurring integer in a given array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,21 +7,17 @@ public class Main {
 
     public static int findSmallestNonOccurring(int[] array) {
         Set<Integer> numbers = new HashSet<>();
-
         for (int num : array) {
             if (num > 0) {
                 numbers.add(num);
             }
         }
-
         int smallestNonOccurring = 1;
         while (numbers.contains(smallestNonOccurring)) {
             smallestNonOccurring++;
         }
-
         return smallestNonOccurring;
     }
-
     //Testing Assessment 1
     public static void main(String[] args) {
         int[] array1 = {1, 3, 6, 4, 1, 2};
